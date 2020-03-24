b7740bdcbbe53d58c9f8bbd2760dada251f0564c
Douglas Walas Rodrigues Pardim <douglas.skd@gmail.com>
2020-03-24 20:58:18 +0000

    add migration configurations

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require( 'mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const routes = require('./routes');
+const http = require('http');
+
+require('dotenv').config();
+const app = express();
+
+app.use(cors({origin:'*'}));
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(process.env.PORT || 8080);
